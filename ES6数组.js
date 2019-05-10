849b0d2950a3448d593355c4a309ee7119bf09a6
hyang <1348261663@qq.com>
2019-05-10 14:45:52 +0800

    Update ES6数组.js

--- ES6数组.js
+++ ES6数组.js
@@ -1,1 +1,87 @@
 
+//1.扩展运算符
+
+//扩展运算符（spread）是三个点（...）。它好比 rest 参数的逆运算，将一个数组转为用逗号分隔的参数序列。
+
+console.log(...[1, 2, 3])
+// 1 2 3
+
+console.log(1, ...[2, 3, 4], 5)
+// 1 2 3 4 5
+
+[...document.querySelectorAll('div')]
+// [<div>, <div>, <div>]
+
+//应用
+//(1)复制数组
+const a1 = [1, 2];
+const a2 = a1;
+
+a2[0] = 2;
+a1 // [2, 2]
+
+//(2)合并数组
+const arr1 = ['a', 'b'];
+const arr2 = ['c'];
+const arr3 = ['d', 'e'];
+
+// ES5 的合并数组
+arr1.concat(arr2, arr3);
+// [ 'a', 'b', 'c', 'd', 'e' ]
+
+// ES6 的合并数组
+[...arr1, ...arr2, ...arr3]
+// [ 'a', 'b', 'c', 'd', 'e' ]
+//不过，这两种方法都是浅拷贝，使用的时候需要注意。
+
+const a1 = [{ foo: 1 }];
+const a2 = [{ bar: 2 }];
+
+const a3 = a1.concat(a2);
+const a4 = [...a1, ...a2];
+
+a3[0] === a1[0] // true
+a4[0] === a1[0] // true
+
+//(3)与解构赋值结合
+
+// ES5
+a = list[0], rest = list.slice(1)
+// ES6
+[a, ...rest] = list
+
+//(4)字符串
+//扩展运算符还可以将字符串转为真正的数组。
+
+[...'hello']
+// [ "h", "e", "l", "l", "o" ]
+
+//(5)实现了 Iterator 接口的对象
+//任何定义了遍历器（Iterator）接口的对象，都可以用扩展运算符转为真正的数组。
+
+let nodeList = document.querySelectorAll('div');
+let array = [...nodeList];
+
+//(6)Map 和 Set 结构，Generator 函数
+//扩展运算符内部调用的是数据结构的 Iterator 接口，因此只要具有 Iterator 接口的对象，都可以使用扩展运算符，比如 Map 结构。
+
+let map = new Map([
+  [1, 'one'],
+  [2, 'two'],
+  [3, 'three'],
+]);
+
+let arr = [...map.keys()]; // [1, 2, 3]
+//Generator 函数运行后，返回一个遍历器对象，因此也可以使用扩展运算符。
+
+const go = function*(){
+  yield 1;
+  yield 2;
+  yield 3;
+};
+
+[...go()] // [1, 2, 3]
+
+
+//2.扩展API：Array.from()
+
