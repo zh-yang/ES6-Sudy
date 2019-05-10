ea6f9a792611aadc0f7aef1fee0365a0c4540d4e
hyang <1348261663@qq.com>
2019-05-10 15:49:18 +0800

    Update ES6数组.js
    数组实例扩展API：includes()

--- ES6数组.js
+++ ES6数组.js
@@ -249,4 +249,21 @@ console.log(entries.next().value); // [0, 'a']
 console.log(entries.next().value); // [1, 'b']
 console.log(entries.next().value); // [2, 'c']
 
+//****************8.数组实例扩展API：includes()
+//Array.prototype.includes方法返回一个布尔值，表示某个数组是否包含给定的值，与字符串的includes方法类似。ES2016 引入了该方法。
+[1, 2, 3].includes(2)     // true
+[1, 2, 3].includes(4)     // false
+[1, 2, NaN].includes(NaN) // true
+//该方法的第二个参数表示搜索的起始位置，默认为0。如果第二个参数为负数，则表示倒数的位置
+//如果这时它大于数组长度（比如第二个参数为-4，但数组长度为3），则会重置为从0开始。
 
+[1, 2, 3].includes(3, 3);  // false
+[1, 2, 3].includes(3, -1); // true
+
+
+[NaN].indexOf(NaN)
+// -1
+//includes使用的是不一样的判断算法，就没有这个问题。
+
+[NaN].includes(NaN)
+// true
