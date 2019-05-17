01626b72c27100fb71bccf1ff776aaf5be5dcbfa
hyang <1348261663@qq.com>
2019-05-17 22:00:31 +0800

    Update 手写promise.js

--- 手写promise.js
+++ 手写promise.js
@@ -1,1 +1,34 @@
 
+class MyPromise {
+  constructor(fn) {
+    //控制状态
+    this.state = 'pending'
+    this.value = null
+    this.error = null
+    
+    //定义resolve函数
+    const resolve = value => {
+      if(this.state === 'pending') {
+        this.value = value
+        this.state = 'fulfilled'
+      }
+    }
+    
+    //定义reject函数
+    const reject = error => {
+      if(this.state === 'pending') {
+        this.error = error
+        this.state = 'rejected'
+      }
+    }
+    
+    //fn方法可能会抛出异常，需要捕获
+    try {
+      //将resolve和reject函数给使用者
+      fn(resolve, reject)
+    } catch (error) {
+      //如果在函数中抛出异常则将它注入reject中
+      reject(error)
+    }
+  }
+}
